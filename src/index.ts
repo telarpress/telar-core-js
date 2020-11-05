2ea3c63472606b5b8e4af73a4c99440d0259047a
Amir Movahedi <amir.gholzam@live.com>
2020-11-05 20:26:11 +0700

    Add export config interface.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { ConfigStore } from './config';
 
-export { setConfig, getConfig } from './config';
+export { setConfig, getConfig, DBType, ConfigStore, CoreConfig } from './config';
 export * as utils from './utils';
 export * as server from './server';
 export * as data from './data';
