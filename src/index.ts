8fbeb858c5dc54a0a6a81c29461be5786d3e8b83
Amir Movahedi <amir.gholzam@live.com>
2020-11-07 21:56:44 +0700

    refactor: export log in telar-core index
    Signed-off-by: Amir Movahedi <amir.gholzam@live.com>

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { ConfigStore } from './config';
+import { LogUtil } from './utils';
 
 export { setConfig, getConfig, DBType, ConfigStore, CoreConfig } from './config';
 export * as utils from './utils';
 export * as server from './server';
 export * as data from './data';
+
+export const log = LogUtil;
 
 export type Initiazlizer = ConfigStore;
