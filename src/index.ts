05cea9bbe58b149168005675ae135b71812d6786
Amir Movahedi <amir.gholzam@live.com>
2020-11-11 13:19:47 +0700

    refactor(index): expand export files

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,11 @@ import { LogUtil } from './utils';
 
 export { setConfig, getConfig, DBType, ConfigStore, CoreConfig } from './config';
 export * as utils from './utils';
+export * from './utils';
 export * as server from './server';
+export * from './server';
 export * as data from './data';
+export * from './data';
 
 export const log = LogUtil;
 
