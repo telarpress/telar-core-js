99293de55ce6645405edb104e287a7c7ffce5581
Amir Movahedi <amir.gholzam@live.com>
2020-12-14 14:44:36 +0700

    chore: remove console

--- src/HostBuilder.ts
+++ src/HostBuilder.ts
@@ -2,7 +2,6 @@ import { IncomingMessage, ServerResponse } from 'http';
 import { Http2ServerRequest, Http2ServerResponse } from 'http2';
 import { IHostBuilder } from './IHostBuilder';
 import Koa from 'koa';
-import { COMMON } from './constants';
 
 export class HostBuilder extends IHostBuilder {
     readonly properties: Record<string, any>;
@@ -14,7 +13,6 @@ export class HostBuilder extends IHostBuilder {
 
     run() {
         return (req: IncomingMessage | Http2ServerRequest, res: ServerResponse | Http2ServerResponse) => {
-            console.log('All routes =>>>', this.properties[COMMON.router].routes().router.stack);
             (this.properties.context as Koa).callback()(req, res);
         };
     }
